88d5eac1db909e4c7dad6715b3d950e95c99ce68
Kevin Lockett <chlockett@msn.com>
2022-01-25 15:56:07 -0600

    kll-preparing to merge new main

--- request_handler.py
+++ request_handler.py
@@ -8,9 +8,9 @@ from views.user import create_user, login_user
 class HandleRequests(BaseHTTPRequestHandler):
     """Handles the requests to this server"""
 
-    def parse_url(self,path):
+    def parse_url(self, path):
         """Parse the url into the resource and id"""
-        path_params = path.split('/')
+        path_params = self.path.split('/')
         resource = path_params[1]
         if '?' in resource:
             param = resource.split('?')[1]
@@ -61,7 +61,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         post_body = json.loads(self.rfile.read(content_len))
         response = ''
-        resource, _ = self.parse_url()
+        resource, _ = self.parse_url(self.path)
 
         if resource == 'login':
             response = login_user(post_body)
