d40aafce2e644ba2d3effb9abeae590e54645b16
Kevin Lockett <64236756+kevinlockett@users.noreply.github.com>
2022-01-26 13:22:09 -0600

    Revert "working on subscriptions not complete yet"

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
 from .post import Post
-from .subscription import Subscription
 from .category import Category
 from .user import User
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,2 @@
 from .post_request import get_all_posts, get_single_post
-from .subscription_request import get_all_subscriptions
 from .category_request import get_all_categories, get_single_category
-
